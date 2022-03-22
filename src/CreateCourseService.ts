386df388b04d3a06552c24258a32e07e4b8df35b
Alana Ragusa <alana.ragusarabello@gmail.com>
2022-03-22 19:43:38 -0300

    added routes.ts and CreateCourseService.ts files

---
+++ src/CreateCourseService.ts
@@ -0,0 +1,12 @@
+
+/*
+name - string
+duration - number (weeks)
+educator - string
+*/
+
+class CreateCourseService {
+    execute(name:string, duration:number, educator: string){
+        console.log(name, duration, educator);
+    }
+}
